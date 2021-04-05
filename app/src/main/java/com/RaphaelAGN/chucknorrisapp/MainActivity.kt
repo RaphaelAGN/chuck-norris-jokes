e900a438a3096e351a3d627f49590bfb9160d4ec
Raphael de Almeida Granha Nogueira <raphaeln@ciandt.com>
2021-04-05 11:38:40 -0300

    update function call and var instances on MainActivity

--- app/src/main/java/com/RaphaelAGN/chucknorrisapp/MainActivity.kt
+++ app/src/main/java/com/RaphaelAGN/chucknorrisapp/MainActivity.kt
@@ -13,38 +13,42 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    var jokeButton: Button? = null
+    var jokeTextView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val jokeTextView: TextView = findViewById(R.id.joke_text_view)
-        jokeTextView.text = "Click the button below to get a joke"
+        jokeTextView = findViewById(R.id.joke_text_view)
 
-        val jokeButton: Button = findViewById(R.id.joke_button)
-        jokeButton.text="New Joke"
+        jokeTextView?.text = "Click the button below to get a joke"
 
-        jokeButton.setOnClickListener(){
-            jokeTextView.text = getRandomJoke(jokeTextView);
+        jokeButton = findViewById(R.id.joke_button)
+        jokeButton?.text="New Joke"
+
+        jokeButton?.setOnClickListener(){
+            getRandomJoke();
         }
     }
 
-    fun getRandomJoke(textView: TextView) : CharSequence? {
+    fun getRandomJoke() {
         val retrofitClient =
             RetrofitClient.getRetrofitInstance("https://api.chucknorris.io/jokes/");
 
-        val endpoint = retrofitClient.create(Endpoint::class.java);
-        val callback = endpoint.getJoke();
+        val endpoint = retrofitClient.create(Endpoint::class.java)
+        val callback = endpoint.getJoke()
 
         callback.enqueue(object : Callback<JokeModel> {
             override fun onFailure(call: Call<JokeModel>, t: Throwable) {
-                Toast.makeText(baseContext, "An error has occurred", Toast.LENGTH_LONG).show();
+                Toast.makeText(baseContext, "An error has occurred", Toast.LENGTH_LONG).show()
             }
 
             override fun onResponse(call: Call<JokeModel>, response: Response<JokeModel>) {
-                textView.text = response.body()?.value.toString();
+                jokeTextView?.text = response.body()?.value.toString()
             }
         })
-        return textView.text;
     }
 
 }
