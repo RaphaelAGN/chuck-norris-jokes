df5b4ef1ac59b8c9f5a28adb7234b9d4a07b77a1
Raphael de Almeida Granha Nogueira <raphaeln@ciandt.com>
2021-04-15 15:40:49 -0300

    Added dependecy injection with koin

--- app/src/main/java/com/RaphaelAGN/chucknorrisapp/viewModels/JokeViewModel.kt
+++ app/src/main/java/com/RaphaelAGN/chucknorrisapp/viewModels/JokeViewModel.kt
@@ -1,15 +1,9 @@
 package com.RaphaelAGN.chucknorrisapp.viewModels
 
-import android.content.Context
-import android.widget.Toast
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.RaphaelAGN.chucknorrisapp.MainActivity
-import com.RaphaelAGN.chucknorrisapp.R
 import com.RaphaelAGN.chucknorrisapp.data.JokeModel
 import com.RaphaelAGN.chucknorrisapp.endpoint.Endpoint
-import com.RaphaelAGN.chucknorrisapp.fragments.MainFragment
 import com.RaphaelAGN.chucknorrisapp.retrofitClient.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
